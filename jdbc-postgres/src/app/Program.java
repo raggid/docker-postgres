946880d6768b5eeb43bc31192224a143ddcc42e6
Antonio <antonioraggid@gmail.com>
2021-01-05 10:57:22 -0400

    Order and Product from database

--- jdbc-postgres/src/app/Program.java
+++ jdbc-postgres/src/app/Program.java
@@ -4,8 +4,12 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import db.DB;
+import entities.Order;
+import entities.OrderStatus;
+import entities.Product;
 
 public class Program {
 
@@ -15,10 +19,28 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_product");
+		ResultSet rs = st.executeQuery("select * from tb_order");
 			
 		while (rs.next()) {
-			System.out.println(rs.getLong("Id") + ", " + rs.getString("Name"));
+			Order o = getOrder(rs);
+			System.out.println(o);
 		}
+	}
+
+	private static Order getOrder(ResultSet rs) throws SQLException {
+		return new Order(
+				rs.getLong("id"), rs.getDouble("latitude"), rs.getDouble("longitude"),
+				rs.getTimestamp("moment").toInstant(), OrderStatus.values()[rs.getInt("status")],
+				new ArrayList<>()
+		);
+	}
+
+
+	private static Product getProduct(ResultSet rs) throws SQLException {
+		return new Product(
+				rs.getLong("id"), rs.getString("name"),
+				rs.getDouble("price"), rs.getString("description"),
+				rs.getString("image_uri")
+		);
 	}
 }
